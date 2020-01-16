c027b1722e6c71e3a13cd170d6fead601070d5d0
Alejandro Endo <alejoendo@hotmail.com>
2020-01-16 13:41:00 -0500

    aws sdk working

--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
 'use strict';
+const AWS = require('aws-sdk');
 
-module.exports.hello = async event => {
-  return {
+module.exports.hello = async (event, context, callback) => {
+  const response = {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Im neither happy nor sad',
         input: event,
-      },
-      null,
-      2
-    ),
+      })
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
